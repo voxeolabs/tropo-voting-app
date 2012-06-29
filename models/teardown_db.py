7e7d8891a2d46c4a1ddcf2d42cfd268ac6d2e37b
Ana Nelson <ana@ananelson.com>
2012-06-28 21:18:46 -0700

    Add 'if exists' to teardown.

--- models/teardown_db.py
+++ models/teardown_db.py
@@ -1,8 +1,8 @@
 from models.db import db
 
 def teardown_db():
-    db.query("DROP TABLE songs")
-    db.query("DROP TABLE votes")
-    db.query("DROP TABLE sessions")
+    db.query("DROP TABLE if exists songs")
+    db.query("DROP TABLE if exists votes")
+    db.query("DROP TABLE if exists sessions")
 
 teardown_db()
