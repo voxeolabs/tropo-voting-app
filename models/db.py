3c467f6862ff3525fd7c9689ea89963187c92aea
Ana Nelson <ana@ananelson.com>
2012-06-28 20:24:32 -0700

    Fix typoe

--- models/db.py
+++ models/db.py
@@ -9,7 +9,7 @@ def parse_db_url():
     return web.database(dbn='postgres', host=params['HOST'], db=params['NAME'], user=params['USER'], pw=params['PASSWORD'])
 
 def initialize_db():
-    db.query("CREATE TABLE songs (number integer PRIMARY KEY")
+    db.query("CREATE TABLE songs (number integer PRIMARY KEY)")
 
 def reset_db():
     db.query("DROP SCHEMA public CASCADE")
